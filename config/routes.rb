fd867ac42e9ccc8a9c7d90e23be07e9ddbad26e1
melkdee <melkdee.designs@gmail.com>
2019-10-14 14:35:38 -0400

    Setup Listings Controller, Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #2019-10-14: Setting up 'listings controller'
+  root 'listings#index'
 end
