43f0ee05dd7222b3e2b4ca0a510fffa4de11c9c8
melkdee <melkdee.designs@gmail.com>
2019-10-17 18:17:24 -0400

    form now accepts entries to DB, woo hoo

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,8 +5,20 @@ class ListingsController < ApplicationController
 		@pagy, @listings = pagy(Listing.all, page: params[:page], items: 3)
 	end
 
-	# 2019-10-17, "Add New Listing"
+	# 2019-10-17, "Add New Listing PAGE"
 	def new
 		@listing = Listing.new
+	end
+
+	#2019-10-17, "Adding a new Listing to DATABASE"
+	def create
+		Listing.create(listing_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def listing_params
+		params.require(:listing).permit(:name, :description, :address)
 	end
 end
