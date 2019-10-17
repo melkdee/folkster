cccb606155abecbfae737d423ed92dba5dc5ea5c
melkdee <melkdee.designs@gmail.com>
2019-10-17 17:26:24 -0400

    rolled back db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_211206) do
+ActiveRecord::Schema.define(version: 2019_10_14_191446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2019_10_17_211206) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "tags"
   end
 
 end
